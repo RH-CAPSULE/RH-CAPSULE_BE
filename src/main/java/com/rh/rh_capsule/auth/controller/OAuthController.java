5af875fad69e2616f7210ead40693b5c29d034c0
Dohun Kim <dohun8758@naver.com>
2024-05-03 15:28:05 +0900

    auth/oauth2: 구글 소셜 로그인 수정

--- src/main/java/com/rh/rh_capsule/auth/controller/OAuthController.java
+++ src/main/java/com/rh/rh_capsule/auth/controller/OAuthController.java
@@ -7,8 +7,13 @@ import com.rh.rh_capsule.auth.infrastructure.RestTemplateOAuthRequester;
 import com.rh.rh_capsule.auth.service.OAuthService;
 import com.rh.rh_capsule.auth.service.dto.OAuthSignInRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
 
 @RestController
 @RequiredArgsConstructor
@@ -33,4 +38,15 @@ public class OAuthController {
         return ResponseEntity.ok(tokens);
 
     }
+//    @GetMapping("/oauth/{provider}/signin")
+//    public ResponseEntity<?> signIn(@RequestParam String code, @PathVariable String provider) {
+//        String redirectUri = oAuthService.getRedirectUri(provider);
+//        OAuthSignInRequest request = new OAuthSignInRequest(redirectUri, code);
+//        OAuthUser oAuthUser = restTemplateOAuthRequester.signIn(request, provider);
+//        TokenResponse tokens = oAuthService.signIn(oAuthUser);
+//        HttpHeaders headers = new HttpHeaders();
+//        headers.setLocation(URI.create("http://localhost:8080/index.html?accessToken=" + tokens.accessToken() + "&refreshToken=" + tokens.refreshToken()));
+//        return new ResponseEntity<>(headers, HttpStatus.SEE_OTHER);
+////        return ResponseEntity.ok(tokens);
+//    }
 }
