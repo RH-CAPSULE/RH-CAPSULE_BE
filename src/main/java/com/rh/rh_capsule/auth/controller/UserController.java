ee0ec0d86e1d79c53883911e11e0bacf01dc3de0
Dohun Kim <dohun8758@naver.com>
2024-05-06 18:49:23 +0900

    feat/user: 유저 상세 정보 조회 api 구현

---
+++ src/main/java/com/rh/rh_capsule/auth/controller/UserController.java
@@ -0,0 +1,20 @@
+package com.rh.rh_capsule.auth.controller;
+
+import com.rh.rh_capsule.auth.controller.dto.UserDetailDTO;
+import com.rh.rh_capsule.auth.service.UserService;
+import com.rh.rh_capsule.auth.support.AuthUser;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+    @GetMapping("/api/user")
+    public ResponseEntity<UserDetailDTO> getUser(@AuthUser Long userId) {
+
+        return ResponseEntity.ok().body(userService.getUser(userId));
+    }
+}
