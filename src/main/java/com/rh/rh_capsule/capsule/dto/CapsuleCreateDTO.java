7394fca5e08d9c00c20f22f6698e92732d7a14a5
Dohun Kim <dohun8758@naver.com>
2024-05-08 01:06:03 +0900

    feat/capsule: 캡슐 생성 DTO 내용 수정

--- src/main/java/com/rh/rh_capsule/capsule/dto/CapsuleCreateDTO.java
+++ src/main/java/com/rh/rh_capsule/capsule/dto/CapsuleCreateDTO.java
@@ -1,11 +1,11 @@
 package com.rh.rh_capsule.capsule.dto;
 
 public record CapsuleCreateDTO(
+        Long userId,
+        Long capsuleBoxId,
         String color,
         String title,
         String content,
-        String writer,
-        Long capsuleBoxId,
-        Boolean isMine
+        String writer
 ) {
 }
