0bc6b8438e02048620eb7ac05dadf147e392f3d9
Dohun Kim <dohun8758@naver.com>
2024-05-09 01:09:09 +0900

    *hotfix: exception 관련 문제 해결
    GlobalExceptionHandler에서 ControllerAdvice -> RestControllerAdvice로 변경

--- src/main/java/com/rh/rh_capsule/exception/GlobalExceptionHandler.java
+++ src/main/java/com/rh/rh_capsule/exception/GlobalExceptionHandler.java
@@ -8,12 +8,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(AuthException.class)
