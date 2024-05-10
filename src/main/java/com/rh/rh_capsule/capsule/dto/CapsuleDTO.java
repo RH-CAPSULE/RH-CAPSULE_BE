02eed493e3c4ea3787e13404787d16066d0359ce
Dohun Kim <dohun8758@naver.com>
2024-05-11 01:38:10 +0900

    feat/0511/dto-entity-field-name: CapsuleDTO 의 id -> capsuleId로 수정

--- src/main/java/com/rh/rh_capsule/capsule/dto/CapsuleDTO.java
+++ src/main/java/com/rh/rh_capsule/capsule/dto/CapsuleDTO.java
@@ -3,7 +3,7 @@ package com.rh.rh_capsule.capsule.dto;
 import java.time.LocalDateTime;
 
 public record CapsuleDTO(
-        Long id,
+        Long capsuleId,
         String color,
         String title,
         String content,
