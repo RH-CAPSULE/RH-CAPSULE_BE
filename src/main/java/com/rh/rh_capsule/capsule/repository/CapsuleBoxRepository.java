70446e4fe8239a2e087f5e7279def5af4411d0f2
Dohun Kim <dohun8758@naver.com>
2024-05-17 02:51:16 +0900

    feat: OpenedAt이 지난 캡슐 박스를 페이지네이션으로 가져오는 메서드 추가, 불필요한 코드삭제
    - CapsuleBoxRepository에 findByUserIdAndOpenedAtBefore 메서드 추가
    - OpenedAt이 지난 사용자의 캡슐 박스를 페이징하여 가져올 수 있도록 함

--- src/main/java/com/rh/rh_capsule/capsule/repository/CapsuleBoxRepository.java
+++ src/main/java/com/rh/rh_capsule/capsule/repository/CapsuleBoxRepository.java
@@ -1,15 +1,19 @@
 package com.rh.rh_capsule.capsule.repository;
 
-import com.rh.rh_capsule.auth.domain.User;
 import com.rh.rh_capsule.capsule.domain.CapsuleBox;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
 public interface CapsuleBoxRepository extends JpaRepository<CapsuleBox, Long> {
-//    List<CapsuleBox> findByUser(User user);
     List<CapsuleBox> findByUserId(Long userId);
     Optional<CapsuleBox> findById(Long id);
+
+    //페이지 네이션
+    Page<CapsuleBox> findByUserIdAndOpenedAtBefore(Long userId, LocalDateTime openedAt, Pageable pageable);
 
 }
