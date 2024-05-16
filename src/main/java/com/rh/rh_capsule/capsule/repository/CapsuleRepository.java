78daccfb61715cda84f79d13cc2632530e68f5d2
Dohun Kim <dohun8758@naver.com>
2024-05-17 02:52:43 +0900

    feat: 캡슐박스 id로 사용자의 캡슐들을 페이지네이션으로 가져오는 메서드 추가, 불필요한 코드삭제
    - CapsuleRepository findByCapsuleBoxId 메서드 추가
    - 캡슐함의 캡슐들을 페이지네이션으로 가져올수 있도록 함

--- src/main/java/com/rh/rh_capsule/capsule/repository/CapsuleRepository.java
+++ src/main/java/com/rh/rh_capsule/capsule/repository/CapsuleRepository.java
@@ -1,10 +1,10 @@
 package com.rh.rh_capsule.capsule.repository;
 
 import com.rh.rh_capsule.capsule.domain.Capsule;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface CapsuleRepository extends JpaRepository<Capsule, Long> {
-    List<Capsule> findByCapsuleBoxId(Long capsuleBoxId);
+    Page<Capsule> findByCapsuleBoxId(Long capsuleBoxId, Pageable pageable);
 }
