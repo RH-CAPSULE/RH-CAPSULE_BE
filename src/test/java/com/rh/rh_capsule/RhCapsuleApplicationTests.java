19064d52238301c77060d68b7a2d25e2d17e1d9e
Dohun Kim <dohun8758@naver.com>
2024-05-21 03:06:46 +0900

    feat: add test

---
+++ src/test/java/com/rh/rh_capsule/RhCapsuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rh.rh_capsule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhCapsuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
