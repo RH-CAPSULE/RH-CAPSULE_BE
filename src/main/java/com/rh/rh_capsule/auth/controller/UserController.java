87905f2619607f45b96f118bf75f3c716cf3abbb
Dohun Kim <dohun8758@naver.com>
2024-05-24 22:19:44 +0900

    feat: 유저 정보 수정 api 구현

--- src/main/java/com/rh/rh_capsule/auth/controller/UserController.java
+++ src/main/java/com/rh/rh_capsule/auth/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.rh.rh_capsule.auth.controller;
 
 import com.rh.rh_capsule.auth.controller.dto.UserDetailDTO;
+import com.rh.rh_capsule.auth.controller.dto.UserUpdate;
 import com.rh.rh_capsule.auth.service.AuthService;
 import com.rh.rh_capsule.auth.service.UserService;
 import com.rh.rh_capsule.auth.support.AuthUser;
@@ -9,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -24,5 +26,10 @@ public class UserController {
     public ResponseEntity<?> resign(@Parameter(hidden = true) @AuthUser Long userId) {
         userService.resign(userId);
         return ResponseEntity.ok().body("회원 탈퇴가 완료되었습니다.");
+    }
+    @PatchMapping("/api/user")
+    public ResponseEntity<?> updateUser(@Parameter(hidden = true) @AuthUser Long userId, UserUpdate userUpdate) {
+        userService.updateUser(userId, userUpdate);
+        return ResponseEntity.ok().body("회원 정보가 수정되었습니다.");
     }
 }
