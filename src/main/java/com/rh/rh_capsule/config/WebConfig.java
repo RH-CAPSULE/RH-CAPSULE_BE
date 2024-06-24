4173c16f94ba1c9c7b320115f7260beda027cba4
Dohun Kim <dohun8758@naver.com>
2024-06-24 21:26:52 +0900

    *hotfix: cors 설정 변경ˆ

--- src/main/java/com/rh/rh_capsule/config/WebConfig.java
+++ src/main/java/com/rh/rh_capsule/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
         // 백엔드의 모든 API 경로에 대해 CORS 설정을 적용합니다.
         registry.addMapping("/**")
                 // 특정 프론트엔드 도메인을 명시적으로 허용
-                .allowedOriginPatterns("https://moment-capsule.web.app") // 또는 허용할 여러 도메인 나열
+                .allowedOriginPatterns("https://moment-capsule.web.app", "https://write-capsule.xyz") // 또는 허용할 여러 도메인 나열
                 // 허용할 HTTP 메서드 지정
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 // JWT 토큰이 포함된 헤더를 허용
