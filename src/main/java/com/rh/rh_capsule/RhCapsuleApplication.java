fd74fa013d68c80982a61ded9856d7608e236f39
Dohun Kim <dohun8758@naver.com>
2025-04-05 04:25:20 +0900

    *hotfix: 스웨거설정

--- src/main/java/com/rh/rh_capsule/RhCapsuleApplication.java
+++ src/main/java/com/rh/rh_capsule/RhCapsuleApplication.java
@@ -5,7 +5,12 @@ import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@OpenAPIDefinition(servers = {@Server(url = "https://write-capsule.xyz", description = "Default Server URL")})
+@OpenAPIDefinition(
+		servers = {
+				@Server(url = "http://dhxxn-sever.ddns.net", description = "배포 서버"),
+				@Server(url = "http://localhost:8080", description = "로컬 개발 서버")
+		}
+)
 @SpringBootApplication
 public class RhCapsuleApplication {
 
