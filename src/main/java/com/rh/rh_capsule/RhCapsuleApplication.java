7ea408cbe3c1183a9a8e6068cd71c4f1f825ce3b
Dohun Kim <dohun8758@naver.com>
2025-09-26 18:49:09 +0900

    chore: swagger 비활성화 및 swagger 경로 변경

--- src/main/java/com/rh/rh_capsule/RhCapsuleApplication.java
+++ src/main/java/com/rh/rh_capsule/RhCapsuleApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @OpenAPIDefinition(
 		servers = {
-				@Server(url = "https://moment-capsule.shop", description = "배포 서버"),
+				@Server(url = "https://time-capsule.dhxxn.dev", description = "배포 서버"),
 				@Server(url = "http://localhost:8080", description = "로컬 개발 서버")
 		}
 )
